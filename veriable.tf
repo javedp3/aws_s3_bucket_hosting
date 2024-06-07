930e2a37c6c78183229551cfc72774da0c0d1f64
javed <javedp3@gmail.com>
2024-06-07 11:23:59 +0530

    Initial commit of Terraform project

---
+++ veriable.tf
@@ -0,0 +1,11 @@
+variable "my_bucket_region" {
+    description = "my default bucket region"
+    type = string
+    default = "us-east-2"
+}
+
+variable "my_bucket_name" {
+    description = "my bucket name"
+    type = string
+    default = "demo-terraform-demo42buckeet"
+}
